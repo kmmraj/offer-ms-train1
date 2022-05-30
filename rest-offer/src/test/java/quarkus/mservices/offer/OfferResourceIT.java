7821ada2ee05f77038971f6b63a12a58d5c39526
Mohanraj Karatadipalayam <mohanraj.karatadipalayam@amadeus.com>
2022-05-30 13:36:47 +0530

    working copy

---
+++ rest-offer/src/test/java/quarkus/mservices/offer/OfferResourceIT.java
@@ -0,0 +1,9 @@
+package quarkus.mservices.offer;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class OfferResourceIT extends OfferResourceTest {
+
+    // Execute the same tests but in native mode.
+}
